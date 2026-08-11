6eca905555a2a5c448b1b99eec7afa844627d78f
Mitchell Tillman <mtillman14@gmail.com>
2026-08-11 11:38:50 -0500

    Docs outline

--- tmp.py
+++ tmp.py
@@ -7,5 +7,19 @@ result = scifor.for_each(
     lambda filepath: str(filepath),
     inputs={"filepath": scifor.EachOf(pi_assessment, pi_training)},
     dry_run=True,
-    subject=[], session=[],
+    subject=[], session=[]
 )
+
+# scifor.Fixed(my_var, session="BL")
+
+# def compute_delta_from_bl(session_val: float, bl_val: float) -> float:
+#     return session_val - bl_val
+
+
+# result = scifor.for_each(compute_delta_from_bl
+#     inputs={
+#         "session_val": session_val, "bl_val": scifor.Fixed(bl_val, session="BL")
+#     },
+#     output_names=["delta_from_bl"],
+#     subject= [], session = []
+# )
