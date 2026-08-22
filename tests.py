257bea42e6657c12d84a75fd0304a3c9e9e66b52
[Mitchell Tillman] <[mtillman14@gmail.com]>
2026-08-22 14:22:04 -0500

    Move dev stuff to tests folder

--- tests.py
+++
@@ -1,6 +0,0 @@
-1. Run all skip_computed tests to ensure we didn't break anything else:                                                                                        
-  python -m pytest scihist/tests/test_skip_computed.py -xvs                                                                                                  
-  2. Run variant/branch_params tests to ensure legitimate variants still work:                                                                                   
-  python -m pytest scidb/tests/test_branch_params.py -xvs                                                                                                        
-  3. Run integration tests:                                                                                                                                      
-  python -m pytest scidb/tests/test_integration.py -xvs
